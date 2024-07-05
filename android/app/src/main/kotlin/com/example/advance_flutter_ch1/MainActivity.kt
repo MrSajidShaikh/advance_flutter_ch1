96742d299d75d1cc5d61c01a701e7d328e0fe78d
sajid <sajidshaikh12543@gmail.com>
2024-07-05 15:39:57 +0530

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/advance_flutter_ch1/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.advance_flutter_ch1
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/Switch dark theme to light theme/theme_change.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+void main()
+{
+  runApp(ThemeChange());
+}
+
+class ThemeChange extends StatelessWidget {
+  const ThemeChange({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
